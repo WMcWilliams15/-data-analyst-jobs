a9cdf1a700a038bf492a2b005d4b609c1a24ee4e
WMcWilliams15 <WMcWilliams15@gmail.com>
2020-01-19 21:39:38 -0600

    Question 3, complete!

--- scripts.sql
+++ scripts.sql
@@ -7,9 +7,17 @@ FROM data_analyst_job;
 Answer 1. 	1793
 
 Question 2. Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
-*/
+
 SELECT *
 FROM data_analyst_job
 LIMIT 10
 
 Answer 2. ExxonMobil
+
+Question 3. How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
+*/
+SELECT COUNT(*)
+FROM data_analyst_job
+WHERE location = 'TN' OR location = 'KY';
+
+Answer TN= 21, KY = 6
