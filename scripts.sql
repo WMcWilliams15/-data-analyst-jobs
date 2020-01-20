4d8f1e4ee736dce70a552f136b8bfe75c0e2353f
WMcWilliams15 <WMcWilliams15@gmail.com>
2020-01-19 19:38:21 -0600

    Question 2, complete!

--- scripts.sql
+++ scripts.sql
@@ -1,6 +1,15 @@
-/*Question 1.	How many rows are in the data_analyst_jobs table?*/
+/*
+Question 1.	How many rows are in the data_analyst_jobs table?
 
 SELECT COUNT(*)
 FROM data_analyst_job;
 
 Answer 1. 	1793
+
+Question 2. Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
+*/
+SELECT *
+FROM data_analyst_job
+LIMIT 10
+
+Answer 2. ExxonMobil
