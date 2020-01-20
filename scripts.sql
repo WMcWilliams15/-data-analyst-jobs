23024cf904e5555ea56143b3352a80a8768bf31c
WMcWilliams15 <WMcWilliams15@gmail.com>
2020-01-19 22:25:39 -0600

    question 7, complete!

--- scripts.sql
+++ scripts.sql
@@ -41,10 +41,18 @@ Answer 5	151
 Question 6	Show the average star rating for each state. 
 			The output should show the state as state and the average rating for the state as avg_rating.
 			Which state shows the highest average rating?
-*/
+
 SELECT avg(star_rating) AS avg_rating, location AS state
 FROM data_analyst_job
 GROUP BY location
 ORDER BY avg_rating DESC;
 
 Answer 6	NE
+
+Question 7	Select unique job titles from the data_analyst_jobs table. How many are there?
+*/
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_job;
+
+Answer 7	881
+
